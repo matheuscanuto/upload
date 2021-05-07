492f07867582aa6a9b797871929c1861a6664dcb
matheuscanuto <83411828+matheuscanuto@users.noreply.github.com>
2021-05-07 20:25:23 -0300

    javascript files

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const multer = require("multer");
+
+app.set('view engine','ejs');
+
+const storage  = multer.diskStorage({
+	destination: function(req,file,cb){
+		cb(null, "uploads/");
+	},
+	filename: function(req,file,cb){
+		cb(null, file.originalname + Date.now());
+	}
+})
+const upload = multer({storage})
+
+app.get('/', (req,res) => {
+	res.render("index");
+})
+app.post('/upload',upload.single("file"),(req,res) => {
+   res.send("arquivo recebido !")
+})
+app.listen(3000, () => {
+	console.log("servidor rodando na porta 3000");
+
+});
